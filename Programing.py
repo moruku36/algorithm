89e6f1eb03080b5019498d47169b23971ce35ee9
Kentaro Mori <morikentaro@Kentaros-MacBook-Air.local>
2025-07-29 12:57:50 +0900

    数字当てゲームの範囲を1-20に変更

--- Programing.py
+++ Programing.py
@@ -1,14 +1,23 @@
 null = 0
-# array = [[2,null,2,null],[2,null,2,null],[null,null,null,null],[null,null,null,null]]
-array = [[4,2,4,2],[4,null,4,2],[2,null,8,2],[16,null,4,null]]
-range = [0,1,2]
-for y in range:
-   for x in range:
-       if array[x+1][y] != null:
-           if array[x+1][y] == array[x][y]:
-               array[x][y] = array[x][y]*2
-               array[x+1][y] = null
-           if array[x][y] == null:
-               array[x][y] = array[x+1][y]
-               array[x+1][y] = null
-print(array)
+import random
+
+answer = random.randint(1, 20)
+
+print("1から20の数字を当ててください！")
+
+while True:
+    guess = input("あなたの予想: ")
+
+    if not guess.isdigit():
+        print("数字を入力してください。")
+        continue
+
+    guess = int(guess)
+
+    if guess == answer:
+        print("正解です！🎉")
+        break
+    elif guess < answer:
+        print("もっと大きいです。")
+    else:
+        print("もっと小さいです。")
